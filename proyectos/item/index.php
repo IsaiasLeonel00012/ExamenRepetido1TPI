a1620bb0342a58ec5bd8efca83c3392bc6ea9f41
IsaiasLeonel00012 <isaiasleonel2004@gmail.com>
2026-09-16 11:47:45 -0600

    cambios docker compose y  html

---
+++ proyectos/item/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <label for="nombre">nombre</label>
+  <input type="nombre" name="">
+
+  <label for="email">email</label>
+  <input type="email">
+
+  <label for=""></label>
+
+
+
+
+</body>
+</html>
